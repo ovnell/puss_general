2880733ae75c0f4a56521a36a9f996179e7e9962
ovnell <niklas_ovnell@hotmail.com>
2015-09-22 22:34:29 +0200

    Added second activity and made the button nagivate there

--- OneOfMyFirstApps/app/src/main/java/se/ovnell/oneofmyfirstapps/MyActivity.java
+++ OneOfMyFirstApps/app/src/main/java/se/ovnell/oneofmyfirstapps/MyActivity.java
@@ -1,11 +1,27 @@
 package se.ovnell.oneofmyfirstapps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.List;
 
 public class MyActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "se.ovnell.oneofmyfirstapps.MESSAGE";
+
+    /** Called when the user clicks the send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
